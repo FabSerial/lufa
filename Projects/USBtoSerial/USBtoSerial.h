ab19543f29f483fbd85a44df28b6c2274baa1676
Zaerc <zaerc@xs4all.nl>
2017-07-16 20:40:10 +0200

    Change USBtoSerial to use the FabSerial Board instead

--- Projects/USBtoSerial/USBtoSerial.h
+++ Projects/USBtoSerial/USBtoSerial.h
@@ -52,16 +52,27 @@
 
 	/* Macros: */
 		/** LED mask for the library LED driver, to indicate that the USB interface is not ready. */
-		#define LEDMASK_USB_NOTREADY      LEDS_LED1
+		#define LEDMASK_USB_NOTREADY	 LEDS_LED2
 
 		/** LED mask for the library LED driver, to indicate that the USB interface is enumerating. */
-		#define LEDMASK_USB_ENUMERATING  (LEDS_LED2 | LEDS_LED3)
+		#define LEDMASK_USB_ENUMERATING	 LEDS_LED1
 
 		/** LED mask for the library LED driver, to indicate that the USB interface is ready. */
-		#define LEDMASK_USB_READY        (LEDS_LED2 | LEDS_LED4)
+		#define LEDMASK_USB_READY	 LEDS_NO_LEDS
 
 		/** LED mask for the library LED driver, to indicate that an error has occurred in the USB interface. */
-		#define LEDMASK_USB_ERROR        (LEDS_LED1 | LEDS_LED3)
+		#define LEDMASK_USB_ERROR	 LEDS_ALL_LEDS
+
+		/** Debug flow control */
+		//#define DEBUG_LEDS
+
+		#ifndef DEBUG_LEDS
+		#define LEDMASK_RX		 LEDS_LED1
+		#define LEDMASK_TX		 LEDS_LED2
+		#else // ! DEBUG_LEDS
+		#define LEDMASK_DSR		 LEDS_LED1
+		#define LEDMASK_DTR		 LEDS_LED2
+		#endif // ! DEBUG_LEDS
 
 	/* Function Prototypes: */
 		void SetupHardware(void);
